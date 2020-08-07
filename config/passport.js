237e25fbb8f0d2224951af43a8d1957a3a7d7af2
fabcodingzest <48706652+fabcodingzest@users.noreply.github.com>
2020-08-08 04:48:08 +0530

    added create, save, update and public fn

--- config/passport.js
+++ config/passport.js
@@ -4,12 +4,30 @@ const User = require('../models/User');
 
 module.exports = function (passport) {
     passport.use(new GoogleStrategy({
-        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackUrl: '/auth/google/callback'
+        callbackURL: '/auth/google/callback'
     },
         async (accessToken, refreshToken, profile, done) => {
-            console.log(profile);
+            const newUser = {
+                googleId: profile.id,
+                displayName: profile.displayName,
+                firstName: profile.name.givenName,
+                lastName: profile.name.familyName,
+                image: profile.photos[0].value,
+            }
+            
+            try {
+                let user = await User.findOne({ googleId: profile.id })
+                if (user) {
+                    done(null, user)
+                } else {
+                    user = await User.create(newUser)
+                    done(null, user)
+                }
+            } catch (err) {
+                console.error(err);
+            }
         }))
 
     passport.serializeUser((user, done) => {
@@ -17,5 +35,6 @@ module.exports = function (passport) {
     });
 
     passport.deserializeUser((id, done) => {
-        User.findById(id, (err, user) => done(err, user))})
+        User.findById(id, (err, user) => done(err, user))
+    })
 }
