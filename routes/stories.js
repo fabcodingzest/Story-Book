04a83ebd76136ec40f7c0d4a76ea0f323468cd13
fabcodingzest <48706652+fabcodingzest@users.noreply.github.com>
2020-08-08 05:42:46 +0530

    App completed with single user stories

--- routes/stories.js
+++ routes/stories.js
@@ -40,6 +40,24 @@ router.get('/', ensureAuth, async (req, res) => {
     }
 })
 
+// @desc Show single story
+// @route GET /stories/:id
+router.get('/:id', ensureAuth, async (req, res) => {
+    try {
+        let story = await Story.findById(req.params.id).populate('user').lean()
+
+        if (!story) {
+            res.render('error/500')
+        }
+        res.render('stories/show', {
+            story
+        })
+    } catch (error) {
+        console.error(error)
+        res.render('error/404')
+    }
+});
+
 // @desc Show edit page
 // @route GET /stories/edit/:id
 router.get('/edit/:id', ensureAuth, async (req, res) => {
@@ -105,6 +123,26 @@ router.delete('/:id', ensureAuth, async (req, res) => {
             res.redirect('/dashboard')
         }
     } catch (err) {
+        console.error(err)
+        return res.render('error/500')
+    }
+});
+
+// @desc User Story
+// @route GET /stories/user/:userId
+router.get('/user/:userId', ensureAuth, async (req, res) => {
+    try {
+        const stories = await Story.find({
+            user: req.params.userId,
+            status: 'public'
+        })
+            .populate('user')
+            .lean()
+
+        res.render('stories/index', {
+            stories,
+        })
+    } catch (error) {
         console.error(err)
         return res.render('error/500')
     }
